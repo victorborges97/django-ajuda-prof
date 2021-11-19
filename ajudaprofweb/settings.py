0315c37d6498129aabcedc2095987955f090e5f1
JV. Borges <borges.jvdo@gmail.com>
2021-11-19 19:03:40 -0300

    consertando problemas no collectionstatic

--- ajudaprofweb/settings.py
+++ ajudaprofweb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -167,3 +168,5 @@ ACCOUNT_UNIQUE_EMAIL = True
 # django-cripy-forms
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+django_on_heroku.settings(locals())
